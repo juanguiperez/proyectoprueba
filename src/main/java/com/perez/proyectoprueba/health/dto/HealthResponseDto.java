06df46ae84f7de121ef910ab066bb022a397ebf5
juanguiperez <juan.caniulef.17@gmail.com>
2026-09-09 10:47:21 -0300

    Primer commit

--- src/main/java/com/perez/proyectoprueba/health/dto/HealthResponseDto.java
+++ src/main/java/com/perez/proyectoprueba/health/dto/HealthResponseDto.java
@@ -3,5 +3,6 @@ package com.perez.proyectoprueba.health.dto;
 public record HealthResponseDto(String status, String error) {
     public  HealthResponseDto(String status) {
         this(status, null );
+        //sad
     }
 }
